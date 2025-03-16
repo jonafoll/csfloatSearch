480e21680df7ccd0cd0fdf942d2b67e26db85aa5
unknown <jonathan.folland@gmail.com>
2025-03-16 15:34:14 +0100

    added search by seed and item id with user input

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
 import requests
+import json
 
 # Load environment variables from .env file
 load_dotenv()
@@ -12,11 +13,29 @@ if not api_key:
 
 url = "https://csfloat.com/api/v1/listings"
 
+# Get user input for paint_index and paint_seed
+paint_index = int(input("Enter the paint_index value: "))
+paint_seed = int(input("Enter the paint_seed value: "))
+
+# Set the parameters with userinput values
+params = {
+    "paint_seed": paint_seed, 
+    "paint_index": paint_index
+}
+
 headers = {
     "Authorization": api_key
 }
 
-response = requests.get(url, headers=headers)
+response = requests.get(url, headers=headers, params=params)
 
+# Print status code
 print("Status Code:", response.status_code)
-print("Response JSON:", response.json())
+
+# Print the JSON response with pretty formatting
+try:
+    # Parse the JSON response and pretty print it
+    json_data = response.json()
+    print(json.dumps(json_data, indent=4, sort_keys=True))
+except ValueError as e:
+    print("Error parsing JSON:", e)
